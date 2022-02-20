bbb91593c8a938966bd4de6131268e5a492f5b22
faldyantono <96801457+faldyantono@users.noreply.github.com>
2022-02-21 02:03:31 +0700

    Add files via upload

---
+++ tugas16.js
@@ -0,0 +1,2 @@
+console.log("Nilai Minimal adalah : ", Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10));
+console.log("Nilai Maximal adalah : ", Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10));
